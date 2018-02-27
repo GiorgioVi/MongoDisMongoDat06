5012bbb4abd669350c158f5f7029df80258d6c9d
Max Z <max.zlotskiy@gmail.com>
2018-02-26 21:44:01 -0500

    wrote basic flask app

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+import create
 
-app = Flask()
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("form.html")
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
+if __name__ == "__main__":
+    create.makeDatabase()
+    app.debug = True
+    app.run()
