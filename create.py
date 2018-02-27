04d1438dabb29a424546d6c71e0d3b9f668f6cd9
Max Z <max.zlotskiy@gmail.com>
2018-02-26 22:20:19 -0500

    fixed error with dropping database:

--- create.py
+++ create.py
@@ -6,7 +6,7 @@ def makeDatabase():
     dictionary = json.loads(file.read())
     file.close()
     connection = pymongo.MongoClient("homer.stuy.edu")
+    connection.drop_database("its2010again")
     db = connection["its2010again"]
-    db.dropDatabase()
     collection = db["deck"]
     collection.insert_many(dictionary["cards"])
