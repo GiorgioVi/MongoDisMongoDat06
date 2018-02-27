9e9754073f94fd5f5eb1760a4bea8dc1a35a2008
Max Z <max.zlotskiy@gmail.com>
2018-02-26 22:51:32 -0500

    feeds search request to results page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import create
+import query
 
 app = Flask(__name__)
 
@@ -9,7 +10,15 @@ def homepage():
 
 @app.route('/results')
 def results():
-    return render_template("results.html")
+    if "rarity" in request.args:
+        rarity = request.args.get("r", "Common")
+        return render_template("results.html", query="Cards that are " + rarity, list=query.get_by_rarity(rarity))
+    elif "cmc" in request.args:
+        min = request.args.get("min", '0')
+        max = request.args.get("max", '10')
+        return render_template("results.html", query="Cards that cost between "+min+" and "+max+" mana", list=query.get_mana_range(min, max))
+    else:
+        return redirect(url_for("homepage"))
 
 if __name__ == "__main__":
     create.makeDatabase()
